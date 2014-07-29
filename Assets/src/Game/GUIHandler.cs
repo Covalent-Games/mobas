c6532237dacbfd3d7f59449958aa38606ecf46da
Justin Smith <jtsmith1287@gmail.com>
2014-07-28 21:38:53 -0700

    Added basic menu framework. 	- Needs a lot of love but you can now: 		Quit from within the game 		Log out to the lobby 		Open an inventory window 			There's nothing to display in the inventory but the framework is there to ease the process

---
+++ Assets/src/Game/GUIHandler.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIHandler : MonoBehaviour {
+
+	public Texture2D crosshair;
+	public float crosshairSize = 10.0f; // Larger number DECREASES size
+
+	private float crosshairPosAdjust;
+	private int screenCenterX;
+	private int screenCenterY;
+	private Rect crosshairRect;
+	
+	// Delegate for assigning Menues to methods
+	// This allows us to assign a method as if it were a variable.
+	public delegate void MenuDelegate();
+	public MenuDelegate DrawOpenMenu;
+	
+	EscapeMenu escapeMenu;
+	Inventory inventoryMenu;
+		
+	void Start () {
+
+		SetCrosshairVariables();
+		escapeMenu = gameObject.AddComponent<EscapeMenu>();
+		inventoryMenu = gameObject.AddComponent<Inventory>();
+	}
+	
+	private void SetCrosshairVariables(){
+	
+		// Center crosshair based on size of image
+		crosshairPosAdjust = crosshairSize/2;
+		// Get the center of the screen
+		screenCenterX = Screen.width/2;
+		screenCenterY = Screen.height/2;
+		// Create new Rect for crosshair
+		crosshairRect = new Rect(screenCenterX-crosshair.width/crosshairSize,
+		                         screenCenterY-crosshair.height/crosshairSize,
+		                         crosshair.width/crosshairPosAdjust, crosshair.height/crosshairPosAdjust);
+	}
+	
+	public void TogglePlayerControls(bool toggle){
+	
+		GameObject player = GameObject.FindGameObjectWithTag("Player");
+		if (player != null){
+			player.transform.Find("CameraPivot").GetComponent<CameraXPivot>().enabled = toggle;
+			player.GetComponent<AvatarMovement>().enabled = toggle;
+		}
+	}
+	
+	private void DrawCrosshair(){
+
+		GUI.DrawTexture(crosshairRect, crosshair);
+	}
+	
+	public void CloseCurrentMenu(){
+		
+		DrawOpenMenu = null;
+		TogglePlayerControls(true);
+	}
+	
+	private void CheckForKeyPress(){
+
+		if (Input.GetKeyDown (KeyCode.Escape)){
+			if (DrawOpenMenu == null){
+				DrawOpenMenu = escapeMenu.DrawMenu;
+			} else {
+				CloseCurrentMenu();
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.I)){
+			DrawOpenMenu = inventoryMenu.DrawMenu;
+		}
+	}
+	
+	void Update(){
+		
+		CheckForKeyPress();
+	}
+	
+	void OnGUI () {
+
+		if (DrawOpenMenu != null){ // If null, no menu is open.
+			DrawOpenMenu();	
+		} else {
+			DrawCrosshair();
+		}
+	}
+}
