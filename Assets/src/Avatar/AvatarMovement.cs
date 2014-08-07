4f67756dfeefad6b910702ebea766f87d4f6e706
Dale Hammack <theophilus602@gmail.com>
2014-08-07 13:01:34 -0700

    tweaked movement a bit, added inertia, momentum, and ground stickyness to allow more control over character of movement and also made it so you can't move while off the ground (thus the previous variables)

--- Assets/src/Avatar/AvatarMovement.cs
+++ Assets/src/Avatar/AvatarMovement.cs
@@ -5,38 +5,48 @@ public class AvatarMovement : MonoBehaviour {
 
 	CharacterController controller;
 	[SerializeField]
-	float movementSpeed = 10.0f;
+	float movementSpeed = 15.0f;
 	[SerializeField]
 	float lookSensitivity = 5.0f;
 	[SerializeField]
 	float diagonalResponsiveness = 0.09f;
+	[SerializeField]
+	float stickynessToGround = 0.09f;
+	[SerializeField]
+	float inertia = 0.981f;
+	
 	float gravity;
 	float lookx;
-
+	float momentumX = 0.0f;
+	float momentumY = 0.0f;
+	
 	void Start () {
 		controller = GetComponent<CharacterController>();
 	}
 	
 	void move(){
 		float time = Time.deltaTime;
+		float moveX = momentumX;
+		float moveY = momentumY;
 		if (controller.isGrounded) {
-			gravity = 0.0f;
+			gravity = -stickynessToGround;
+		
+			moveX = Input.GetAxis("Horizontal") * movementSpeed * time;
+			moveY = Input.GetAxis("Vertical") * movementSpeed * time;
+	
+			// Diagonal movement compensation
+			if (Mathf.Abs(moveX) > diagonalResponsiveness && Mathf.Abs(moveY) > diagonalResponsiveness){
+				float diagonal_move = Mathf.Sqrt(moveX * moveX + moveY * moveY);
+				moveX = diagonal_move / 2 * Mathf.Sign(moveX);
+				moveY = diagonal_move / 2 * Mathf.Sign(moveY);
+			}
 		} else {
 			gravity -= 9.81f * time * time;
 		}
-
-		float moveX = Input.GetAxis("Horizontal") * movementSpeed * time;
-		float moveY = Input.GetAxis("Vertical") * movementSpeed * time;
-
-		// Diagonal movement compensation
-		if (Mathf.Abs(moveX) > diagonalResponsiveness && Mathf.Abs(moveY) > diagonalResponsiveness){
-			float diagonal_move = Mathf.Sqrt(moveX * moveX + moveY * moveY);
-			moveX = diagonal_move / 2 * Mathf.Sign(moveX);
-			moveY = diagonal_move / 2 * Mathf.Sign(moveY);
-		}
-
+		momentumX = moveX * inertia;
+		momentumY = moveY * inertia;
+		
 		Vector3 direction = new Vector3 (moveX, gravity, moveY);
-
 		controller.Move(transform.TransformDirection(direction));
 	}
 
