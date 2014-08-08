a548b934986694f4fc19d4a6a4e30df46343b51b
Justin Smith <jtsmith1287@gmail.com>
2014-08-07 19:24:28 -0700

    Removed ugly black screen and made some 'sparks'

--- Assets/src/Menu/MainMenuGui.cs
+++ Assets/src/Menu/MainMenuGui.cs
@@ -3,8 +3,7 @@ using System.Collections;
 
 public class MainMenuGui : MonoBehaviour {
 
-	[SerializeField]
-	Texture2D _mainMenuBG;
+
 	[SerializeField]
 	Texture2D _startGameButton;
 	[SerializeField]
@@ -16,7 +15,6 @@ public class MainMenuGui : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), _mainMenuBG);
 		
 		GUILayout.BeginArea(_buttonLayoutRect);
 		
