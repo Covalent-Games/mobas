0c1f41728d487cb558c66da3e07c7954e9b8bab2
Justin Smith <jtsmith1287@gmail.com>
2014-08-08 20:55:50 -0700

    Merging demo into master and deleting

---
+++ Assets/MusicLoop.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicLoop : MonoBehaviour {
+
+	private string level;
+	
+	void Awake(){
+		// If the scene is new, set the object to remain.
+		// According to the intertubes this should work. It doesn't. -_-
+		if (Application.loadedLevelName != level){
+			level = Application.loadedLevelName;
+			DontDestroyOnLoad(transform);
+			print("Music shall remain...");
+		}
+	}
+}
