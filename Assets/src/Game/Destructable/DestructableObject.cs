4baf2c2df9c69d7ac57daadba34c99fa3c8570cb
Justin Smith <jtsmith1287@gmail.com>
2014-08-26 23:04:09 -0700

    Forgot to add new files

---
+++ Assets/src/Game/Destructable/DestructableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableObject : MonoBehaviour {
+
+	#region Inheritable Members
+	protected int maxHealth;
+	protected int health;
+
+	public int Health {
+		get{
+			return this.health;
+		}
+		set{
+			if (this.health + value > this.maxHealth){
+				this.health = this.maxHealth;
+			} else if(this.health - value <= 0){
+				this.health = 0;
+				CheckIfDestroyed();
+			}
+		}
+	}
+	
+	#endregion
+	
+	#region Inheritable Methods
+	protected override void CheckIfDestroyed(){
+		//TODO Destroy logic in case 0 health doesn't actually mean 'dead'
+		Destroy(gameObject);
+	}
+	#endregion
+}
