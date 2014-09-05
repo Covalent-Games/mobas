6d5229d0c27e4da3f6d89db349663d5e198c3ba3
Jesse Pitel <jesse.pitel@gmail.com>
2014-09-04 17:13:10 -1000

    sync

---
+++ Assets/src/Game/Destructable/MobileObject/Player/Character/Character.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : PlayerObject {
+
+	[SerializeField]
+	PhotonView photonView;
+	
+	void Start () {
+	
+		name = gameObject.GetInstanceID().ToString();
+		base.Start();
+		if(PhotonNetwork.isMasterClient) {
+			gameObject.AddComponent<MasterRpcList>();
+		}
+	}
+	
+	void Update () {
+	
+		base.Update();
+	}
+	
+	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo messageInfo){
+		
+		if (stream.isWriting){
+			stream.SendNext(transform.position);
+			stream.SendNext(transform.rotation);
+		} else {
+			transform.position = (Vector3)stream.ReceiveNext();
+			transform.rotation = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
---
+++ Assets/src/Game/Destructable/Structure/Tower/TowerObject.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TowerObject : StructureObject {
+
+	float counter = 0f;
+	float shotInterval = 2.0f;
+	int damage = 8;
+	List<GameObject> targeted = new List<GameObject>();
+
+
+	// Use this for initialization
+	void Start () {
+		this.maxHealth = 200;
+		this.Health = 200;
+		SetRadius (10);
+		base.Start ();
+	}
+
+	void shoot(int damage, GameObject target) {
+
+	}
+
+	void OnTriggerEnter(Collider collider) {
+
+		targeted.Add (collider.gameObject);
+		Debug.Log ("--Added " + collider.gameObject.name);
+	}
+
+	void OnTriggerStay(Collider collider) {
+
+		if(counter >= shotInterval) {
+			GameObject getsShot = collider.gameObject;
+			float distance = Mathf.Infinity;
+			Vector3 position = transform.position;
+			Debug.Log("--List length: " + targeted.Count);
+			foreach(var target in targeted) {
+				if(target == null) {
+					targeted.Remove(target);
+				} else {
+					Debug.Log("--Check " + target.name);
+					Vector3 separation = target.transform.position - position;
+					float currentDistance = separation.sqrMagnitude;
+					if(currentDistance < distance) {
+						getsShot = target;
+						currentDistance = distance;
+					}
+				}
+			}
+			Vector3 towerVector = transform.position;
+			Vector3 playerVector = getsShot.transform.position;
+			Debug.Log("-----Shoots?");
+			Debug.DrawLine (towerVector, playerVector, Color.red, 0.25f);
+			shoot (damage, getsShot);
+			counter = 0f;
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+
+		targeted.Remove (collider.gameObject);
+		Debug.Log ("--Removed " + collider.gameObject.name);
+	}
+
+	void UpdateShotCounter() {
+
+		counter += Time.deltaTime;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		UpdateShotCounter ();
+	}
+}
